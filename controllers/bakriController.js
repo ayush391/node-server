b75332663010d67d52e14e3f4355e0ab811f50b9
ayush391 <stringakaak@gmail.com>
2023-01-14 22:17:34 +0530

    added start script

--- controllers/bakriController.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    home: (req, res) => {
-        res.send('hello kaise ho 💋💋💋💋')
-    },
-    bakris: (req, res) => {
-        const bakris = '🐐'.repeat(req.params.count)
-        res.send(' ' + bakris)
-    }
-}
